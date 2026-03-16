a053900251c177bb090dcd811e9f3aec7ac4e102
Nguyễn Thị Trâm Đan <164111410+Tramdanne@users.noreply.github.com>
2026-03-16 10:05:55 +0700

    chinh sua 16/3/2026

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="vi">
-      <body className="gap-2 flex flex-col items-center justify-center border rounded-lg mt-10">
+      <body className="gap-2 flex flex-col items-center justify-center rounded-lg mt-10">
         <Header />
         <main className="w-250 flex justify-center items-center">
           {children}
